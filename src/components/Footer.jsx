ce5207b1a86a0dc8e1fef49660c30d33b268638d
Unbornmoral <alanwokoc@gmail.com>
2025-11-26 06:55:21 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 // src/components/Footer.jsx
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-4 mt-10">
+    <footer className="bg-gradient-to-r from-blue-300 to-teal-600 rounded-xl shadow-2xl overflow-hidden border border-gray-200 text-white font-semibold">
       <div className="container mx-auto text-center text-sm">
         <p>
           © {new Date().getFullYear()} Resume Genius. Built by Alanwoko Chikanma.
@@ -10,7 +10,7 @@ export default function Footer() {
           For any problem or concern, please contact:{" "}
           <a
             href="mailto:alanwokoc@gmail.com"
-            className="text-blue-400 hover:underline"
+            className="text-blue-800 hover:underline"
           >
             alanwokoc@gmail.com
           </a>
